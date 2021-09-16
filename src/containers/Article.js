763554f444e5d858077ba67d43fd19b369f9275f
Jeongin Yoon <jy3524@nyu.edu>
2021-09-16 09:50:08 -0400

    Add Articles and CSS

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Article() {
-    const articlesTitle = `Single Articles`;
-    return (
-        <div>
-            <h1>{articlesTitle}</h1>
-        </div>
-    );
+  const articlesTitle = `Single Articles`;
+  return (
+    <div>
+      <h1>{articlesTitle}</h1>
+    </div>
+  );
 }
 
 export default Article;
--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,12 +1,18 @@
 import React from "react";
+import ArticleCard from "../components/ArticleCard";
+import Data from "../components/data.json";
 
 function ArticleListing() {
-    const articlesTitle = `Articles`;
-    return (
-        <div className="PageWrapper">
-            <h1 className="HeaderOneStyle">{articlesTitle}</h1>
-        </div>
-    );
+  const articlesTitle = `Articles`;
+  return (
+    <div className="PageWrapper">
+      <h1 className="HeaderOneStyle ArticleListingHeader">{articlesTitle}</h1>
+      
+      {Data.map((article, key) => (
+        <ArticleCard article={article} key={key} />
+      ))}
+    </div>
+  );
 }
 
 export default ArticleListing;
